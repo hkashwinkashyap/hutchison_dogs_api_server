bbf97d11caa6de9fd0303827311460f3bede5c1c
hkashwinkashyap <hkashwinkashyap@gmail.com>
2023-10-11 10:39:31 +0100

    initial setup for server

---
+++ dogsAPI/src/test/java/com/hutchison/dogsAPI/DogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutchison.dogsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
